7e656791e77ae38158163e94d0564925d9a81b03
Admin <Admin@DESKTOP-C49GG1B>
2023-03-09 17:28:26 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp6/LaskOut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class LaskOut
+    {
+        public static void Main()
+        {
+            int h = 0;
+            try
+            {
+                int m = h / 0;
+                
+            }
+            finally
+            {
+                Console.WriteLine("Деление на 0 запрещено,осел!:");
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите количество часов:");
+int h = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество минут:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine(Seconds(h, m));
+Console.WriteLine(Lask(h, m));
+int Seconds(int h, int m)
+{
+    return h * 3600 + m * 60;
+}
+
+int Lask(int h, int m)
+{
+    if (h < 0 || m < 0) throw new Exception("Часы или минуты отрицательными быть не могут");
+    return h * 3600 + m * 60;
+}
+class LaskOut
+{
+
+}
